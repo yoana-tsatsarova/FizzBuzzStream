730bbfea1fb760c282e3ef9944ce8425ccbc458f
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-05-19 14:44:48 +0200

    finished and refactored

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,15 +4,20 @@ import java.util.stream.IntStream;
 
 
 public class FizzBuzz {
+    private final int MAX_RANGE = 100;
+    private final int MIN_RANGE = 1;
+    private final int FIZZ_NUM = 3;
+    private final int BUZZ_NUM = 5;
 
 
     public List<String> fizzBuzz() {
-        List numbersTo100 = IntStream.rangeClosed(1, 100)
+
+        List numbersTo100 = IntStream.rangeClosed(MIN_RANGE, MAX_RANGE)
                 .boxed()
                 .map(number -> {
-                    if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
-                    else if (number % 3 == 0) return "Fizz";
-                    else if (number % 5 == 0) return "Buzz";
+                    if (number % FIZZ_NUM == 0 && number % BUZZ_NUM == 0) return "FizzBuzz";
+                    else if (number % FIZZ_NUM == 0) return "Fizz";
+                    else if (number % BUZZ_NUM == 0) return "Buzz";
                     else return Integer.toString(number);
 
                 }).collect(Collectors.toList());
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -7,7 +7,32 @@ class FizzBuzzTest {
     FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    void shouldReturnFizzBuzzfor15(){
-        assertEquals("FizzBuzz",fizzBuzz.fizzBuzz().get(0));
+    void shouldReturnFizzBuzzFor15(){
+        assertEquals("FizzBuzz",fizzBuzz.fizzBuzz().get(14));
+    }
+
+    @Test
+    void shouldReturnFizzFor3(){
+        assertEquals("Fizz",fizzBuzz.fizzBuzz().get(2));
+    }
+    @Test
+    void shouldReturn1For1(){
+        assertEquals("1",fizzBuzz.fizzBuzz().get(0));
+    }
+    @Test
+    void shouldReturnBuzzFor5(){
+        assertEquals("Buzz",fizzBuzz.fizzBuzz().get(4));
+    }
+    @Test
+    void shouldReturnBuzzFor35(){
+        assertEquals("Buzz",fizzBuzz.fizzBuzz().get(34));
+    }
+    @Test
+    void shouldReturnBuzzFor100(){
+        assertEquals("Buzz",fizzBuzz.fizzBuzz().get(99));
+    }
+    @Test
+    void shouldReturnFizzFor66(){
+        assertEquals("Fizz",fizzBuzz.fizzBuzz().get(65));
     }
 }
