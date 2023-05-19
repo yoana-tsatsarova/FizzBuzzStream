f4bcfbac76938f9205019781fac1ec06b0582c05
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-05-19 14:11:13 +0200

    finished fizzbuzz stream

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,15 +1,27 @@
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 
 public class FizzBuzz {
 
-    public static List<Integer> fizzBuzz () {
-        List numbersTo100 = IntStream.rangeClosed(1, 100).boxed().toList();
+
+    public List<String> fizzBuzz() {
+        List numbersTo100 = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .map(number -> {
+                    if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+                    else if (number % 3 == 0) return "Fizz";
+                    else if (number % 5 == 0) return "Buzz";
+                    else return Integer.toString(number);
+
+                }).collect(Collectors.toList());
+
         return numbersTo100;
     }
 
     public static void main(String[] args) {
-        System.out.println(fizzBuzz());
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz());
     }
-
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,5 +6,8 @@ class FizzBuzzTest {
 
     FizzBuzz fizzBuzz = new FizzBuzz();
 
-
+    @Test
+    void shouldReturnFizzBuzzfor15(){
+        assertEquals("FizzBuzz",fizzBuzz.fizzBuzz().get(0));
+    }
 }
